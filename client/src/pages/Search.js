aae3593ed689a91bf9b402e9cfcc01f466470d10
Elizabeth Reuter <elizabeth.j.reuter@gmail.com>
2020-11-30 20:57:19 -0500

    title adjusted

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container'
 import Jumbotron from "../components/Jumbotron";
 import Nav from "../components/Nav";
 
-
 function Search() {
   return (
     <Container fluid>
