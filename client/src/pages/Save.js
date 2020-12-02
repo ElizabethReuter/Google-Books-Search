faa9f10fa8c9210bf601750ff2c5dd29967056fb
Elizabeth Reuter <elizabeth.j.reuter@gmail.com>
2020-12-01 19:16:51 -0500

    components adjusted

--- client/src/pages/Save.js
+++ client/src/pages/Save.js
@@ -2,12 +2,14 @@ import React from "react";
 import Container from 'react-bootstrap/Container'
 import Jumbotron from "../components/Jumbotron";
 import Nav from "../components/Nav";
+import Saved from "../components/Saved";
 
 function Save() {
   return (
     <Container fluid>
         <Nav />
         <Jumbotron /> 
+        <Saved />
     </Container>
   );
 }
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -2,12 +2,16 @@ import React from "react";
 import Container from 'react-bootstrap/Container'
 import Jumbotron from "../components/Jumbotron";
 import Nav from "../components/Nav";
+import Search from "../components/Search";
+import Results from "../components/Results";
 
 function Search() {
   return (
     <Container fluid>
         <Nav />
         <Jumbotron /> 
+        <Seach />
+        <Results />
     </Container>
   );
 }
